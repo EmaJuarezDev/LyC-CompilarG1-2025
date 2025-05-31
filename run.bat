759f7e9b5c2d81426aefe48b1483ab29a3d5a050
jialonso1993 <63207863+jialonso1993@users.noreply.github.com>
2025-05-31 07:53:27 -0300

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,16 @@
+:: Script para windows
+flex Lexico.l
+bison -dyv Sintactico.y
+
+gcc.exe main.c tablaDeSimbolos.c funcionesEspeciales.c lex.yy.c y.tab.c -o lyc-compiler-2.0.0.exe
+
+lyc-compiler-2.0.0.exe test.txt
+
+@echo off
+del lyc-compiler-2.0.0.exe
+del lex.yy.c
+del y.tab.c
+del y.tab.h
+del y.output
+
+pause
